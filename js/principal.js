33c7b568cfebc8e94568279c78bef056aca9fb59
augustoinacio <augustoinacio@gmail.com>
2017-09-20 22:51:28 -0300

    Adicionar form e regra botao

--- js/principal.js
+++ js/principal.js
@@ -36,4 +36,9 @@ for(var i=0; i < pacientes.length; i++){
     }
     console.log(imc);
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function() {
+    console.log("Oi, cliquei no botão.");
+});
     
