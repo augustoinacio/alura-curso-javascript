fc0600250465fe2923c4deb0699e6cd330a24c56
augustoinacio <augustoinacio@gmail.com>
2017-09-25 18:32:12 -0300

    Adiciona filtros

--- js/filtrar.js
+++ js/filtrar.js
@@ -1,10 +1,23 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 campoFiltro.addEventListener("input", function(){
     var pacientes = document.querySelectorAll(".paciente");
-    for (var index = 0; index < pacientes.length; index++) {
-        var paciente = pacientes[index];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
-        
+    console.log("input filtro " + this.value  );
+    if(this.value.length > 0){
+        for (var index = 0; index < pacientes.length; index++) {
+            var paciente = pacientes[index];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+            if(expressao.test(nome)){
+                paciente.classList.remove("invisivel");
+            }else{
+                paciente.classList.add("invisivel");
+            }
+        }
+    }else{
+        for (var index = 0; index < pacientes.length; index++) {
+            var paciente = pacientes[index];
+            paciente.classList.remove("invisivel");
+        }
     }
 });
